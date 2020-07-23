df0788b3505f72c16e5d4138be6ae6c90daef58b
Matej Duracka <watofundefined@o.o0.0>
2020-07-23 22:27:33 +0300

    Improve test coverage by 5 lines in common

--- test/common.js
+++ test/common.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Code = require('@hapi/code');
+const Joi = require('@hapi/joi');
 const Lab = require('@hapi/lab');
 
 const Common = require('../lib/common');
@@ -20,6 +21,25 @@ describe('Common', () => {
         it('validates null', () => {
 
             expect(() => Common.assertOptions()).to.throw('Options must be of type object');
+        });
+    });
+
+    describe('validateArg', () => {
+
+        it('returns nothing if valid', () => {
+
+            const value = 1234;
+            const assert = Joi.number();
+
+            expect(Common.validateArg(value, null, { assert })).to.not.exist();
+        });
+
+        it('returns error message if invalid', () => {
+
+            const value = 1234.5;
+            const assert = Joi.number().integer();
+
+            expect(Common.validateArg(value, null, { assert })).to.be.a.string();
         });
     });
 });
