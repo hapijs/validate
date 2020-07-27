5fbde7515a37873537019715c860e86e4360e9d9
Wes Tyler <wtyler@xogrp.com>
2020-07-27 16:15:31 -0500

    100% coverage on lib/messages.js (#4)

--- test/common.js
+++ test/common.js
@@ -5,6 +5,7 @@ const Lab = require('@hapi/lab');
 
 const Joi = require('..');
 const Common = require('../lib/common');
+const Template = require('../lib/template');
 
 
 const internals = {};
@@ -40,6 +41,48 @@ describe('Common', () => {
             const assert = Joi.number().integer();
 
             expect(Common.validateArg(value, null, { assert })).to.equal('"value" must be an integer');
+        });
+    });
+
+    describe('preferences', () => {
+
+        it('uses the provided messages value', () => {
+
+            const { messages } = Common.preferences({}, { messages: 'custom value' });
+            expect(messages.source).to.equal('custom value');
+        });
+
+        it('uses the provided messages template value', () => {
+
+            const messageTemplate = new Template('custom template value');
+            const { messages } = Common.preferences({}, { messages: messageTemplate });
+            expect(messages.source).to.equal('custom template value');
+        });
+
+        it('uses the provided messages value by language', () => {
+
+            const messageByLanguage = { english: { 'number.min': 'custom message for number.min' } };
+            const defaultMessagesByLanguage = { english: { 'number.min': 'default message for number.min' } };
+            const { messages } = Common.preferences({ messages: defaultMessagesByLanguage }, { messages: messageByLanguage });
+            expect(messages.english['number.min'].source).to.equal('custom message for number.min');
+
+        });
+
+        it('throws with an invalid messages value', () => {
+
+            expect(() => Common.preferences({ messages: [] }, { messages: 'single string error' })).to.throw('Cannot set single message string');
+        });
+
+        it('throws with an invalid messages template value', () => {
+
+            const messageTemplate = new Template('custom template value');
+            expect(() => Common.preferences({ messages: [] }, { messages: messageTemplate })).to.throw('Cannot set single message template');
+        });
+
+        it('throws with invalid messages options', () => {
+
+            expect(() => Common.preferences({ messages: [] }, { messages: true })).to.throw('Invalid message options');
+            expect(() => Common.preferences({ messages: [] }, { messages: { code: true } })).to.throw('Invalid message for code');
         });
     });
 });
